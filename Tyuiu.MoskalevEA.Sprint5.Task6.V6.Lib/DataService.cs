80230e67c60b314f29cfee2dc7cd8618888e6b8f
Егор Москалев <moskalevegor310@gmail.com>
2025-12-07 20:58:11 +0500

    Task6

---
+++ Tyuiu.MoskalevEA.Sprint5.Task6.V6.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.DatskiyDE.Sprint5.Task6.V6.Lib
+{
+    public class DataService : ISprint5Task6V6
+    {
+        public int LoadFromDataFile(string path)
+        {
+
+            string content = File.ReadAllText(path);
+
+            int count = 0;
+
+            foreach (char c in content)
+            {
+                if (c >= 'а' && c <= 'я')
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint5.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MoskalevEA.Sprint5.Task6.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
