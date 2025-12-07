34b192fdb010be657bafdc1eda3732eae60fa559
Егор Москалев <moskalevegor310@gmail.com>
2025-12-07 19:26:03 +0500

    Task2

---
+++ Tyuiu.MoskalevEA.Sprint5.Task2.V30.Lib/DataService.cs
@@ -0,0 +1,56 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MoskalevEA.Sprint5.Task2.V30.Lib
+{
+    public class DataService : ISprint5Task2V30
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] < 0)
+                        matrix[i, j] = 1;
+                    else
+                        matrix[i, j] = 0;
+                }
+            }
+            string str = "";
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j != columns - 1)
+                    {
+                        str = str + matrix[i, j] + ";";
+                    }
+                    else
+                    {
+                        str = str + matrix[i, j];
+                    }
+                }
+                if (i != rows - 1)
+                {
+                    File.AppendAllText(path, str + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, str);
+                }
+                str = "";
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint5.Task2.V30.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MoskalevEA.Sprint5.Task2.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
