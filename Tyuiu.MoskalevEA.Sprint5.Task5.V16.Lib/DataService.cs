ba667c6653adc1767f96c71c5623c08a97d4a5c7
Егор Москалев <moskalevegor310@gmail.com>
2025-12-07 20:55:02 +0500

    Task5

---
+++ Tyuiu.MoskalevEA.Sprint5.Task5.V16.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MoskalevEA.Sprint5.Task5.V16.Lib
+{
+    public class DataService : ISprint5Task5V16
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double m = -100000;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line = reader.ReadLine();
+                string[] lines = line.Split(' ');
+                foreach (string data in lines)
+                {
+                    string temp = data.Replace(".", ",");
+                    double num = Math.Round(Convert.ToDouble(temp), 3);
+                    if (num > m && num % 10 == 0 && num != 0)
+                    {
+                        m = num;
+                    }
+                }
+            }
+            return m;
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint5.Task5.V16.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MoskalevEA.Sprint5.Task5.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
