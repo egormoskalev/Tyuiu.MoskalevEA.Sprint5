0acfecc9efdf031f98ee72dcaa74199cf7ab484a
Егор Москалев <moskalevegor310@gmail.com>
2025-12-07 20:41:16 +0500

    Task3

---
+++ Tyuiu.MoskalevEA.Sprint5.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MoskalevEA.Sprint5.Task3.V14.Lib
+{
+    public class DataService : ISprint5Task3V14
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+            double result;
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                result = Math.Round((4 * Math.Pow(x, 3)) / (Math.Pow(x, 3) - 1), 3);
+                writer.Write(result);
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint5.Task3.V14.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MoskalevEA.Sprint5.Task3.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
