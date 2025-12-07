8f4d570e590e95fe75dc9cad6b76a4a6be79fa90
Егор Москалев <moskalevegor310@gmail.com>
2025-12-07 20:44:30 +0500

    Task4

---
+++ Tyuiu.MoskalevEA.Sprint5.Task4.V15.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MoskalevEA.Sprint5.Task4.V15.Lib
+{
+    public class DataService : ISprint5Task4V15
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            strX = strX.Trim().Replace(".", ",");
+            double res = Math.Sin(Convert.ToDouble(strX)) + (Math.Pow(Convert.ToDouble(strX), 2) / 2);
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint5.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MoskalevEA.Sprint5.Task4.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
