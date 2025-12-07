b5d75cecdae523daf5f164f3a2ea23f4efcfff66
Егор Москалев <moskalevegor310@gmail.com>
2025-12-07 17:28:27 +0500

    Task0

---
+++ Tyuiu.MoskalevEA.Sprint5.Task0.V5.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MoskalevEA.Sprint5.Task0.V5.Lib
+{
+    public class DataService : ISprint5Task0V5
+    {
+
+        public string SaveToFileTextData(int x)
+        {
+            double y = Math.Log((x + 1) / (x + 2));
+            y = Math.Round(y, 3);
+
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            File.WriteAllText(path, y.ToString());
+
+            return path;
+
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint5.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MoskalevEA.Sprint5.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
